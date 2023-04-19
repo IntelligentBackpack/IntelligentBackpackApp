50d7bfbfd68227fe9d1a8bae9cfb3e1398c01879
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-04-19 10:05:01 +0200

    build: setting build

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.application) apply false
+    alias(libs.plugins.library) apply false
+    alias(libs.plugins.android) apply false
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.qa)
+    alias(libs.plugins.taskTree)
+}
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.build.gradle)
+        classpath(libs.gradle.plugin)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,38 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version "3.11.1"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.23"
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
+gitHooks {
+    preCommit {
+        tasks("ktlintCheck")
+    }
+    commitMsg { conventionalCommits() }
+    createHooks(true)
+}
+
+rootProject.name = "IntelligentBackpackApp"
+include(":app")
