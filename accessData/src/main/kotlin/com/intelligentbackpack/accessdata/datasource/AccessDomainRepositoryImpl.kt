6cf92bdf2d025d974698004f435da1cc5520c0d0
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-04-28 15:16:42 +0200

    feat(access): create implementation for AccessDomainRepository

---
+++ accessData/src/main/kotlin/com/intelligentbackpack/accessdata/datasource/AccessDomainRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.intelligentbackpack.accessdata.datasource
+
+import com.intelligentbackpack.accessdomain.entities.User
+import com.intelligentbackpack.accessdomain.repository.AccessDomainRepository
+
+class AccessDomainRepositoryImpl(
+    private val accessLocalDataStorage: AccessLocalDataStorage,
+    private val accessRemoteDataStorage: AccessRemoteDataStorage
+) : AccessDomainRepository {
+    override fun createUser(user: User): User = accessRemoteDataStorage.createUser(user)
+
+    override fun isUserLogged(): Boolean = accessLocalDataStorage.isUserSaved()
+
+    override fun loginWithData(username: String, password: String): User =
+        accessRemoteDataStorage
+            .accessWithData(username, password)
+            .also { accessLocalDataStorage.saveUser(it) }
+
+    override fun automaticLogin(): User =
+        accessLocalDataStorage.getUser()
+
+
+    override fun logoutUser() =
+        accessLocalDataStorage.deleteUser()
+
+}
