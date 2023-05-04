bd147b86ec4beceda0d8902ad053edf71f0f72af
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-04 03:38:36 +0200

    fix(app): In Login, run automatic login only the first time the page is loaded

--- app/src/main/kotlin/com/intelligentbackpack/app/view/Login.kt
+++ app/src/main/kotlin/com/intelligentbackpack/app/view/Login.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -48,8 +49,10 @@ fun Login(
         factory = LoginViewModel.Factory,
     )
 ) {
-    loginViewModel.tryAutomaticLogin {
-        navController.navigate(MainNavigation.home)
+    LaunchedEffect(Unit) {
+        loginViewModel.tryAutomaticLogin {
+            navController.navigate(MainNavigation.home)
+        }
     }
     LoginPage(navController, loginViewModel::login)
 }
