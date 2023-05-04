731889a08a6611ebebeef2fa0310905a8400402d
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-04 03:33:10 +0200

    build(app): Add dependency runtime-livedata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.runtime:runtime-livedata")
     implementation(libs.core.ktx)
     implementation(project(":accessData"))
     implementation(project(":accessDomain"))
