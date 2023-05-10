22db92e6df62b3782756a547f07244b5e1d3e95a
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-11 00:00:36 +0200

    refactor(app): change names following changes of commit 2ec19fa1

--- app/src/main/kotlin/com/intelligentbackpack/app/di/ServiceLocator.kt
+++ app/src/main/kotlin/com/intelligentbackpack/app/di/ServiceLocator.kt
@@ -2,8 +2,8 @@ package com.intelligentbackpack.app.di
 
 import android.content.Context
 import com.intelligentbackpack.accessdata.datasource.AccessDomainRepositoryImpl
-import com.intelligentbackpack.accessdata.datasource.AccessLocalDataStorageImpl
-import com.intelligentbackpack.accessdata.datasource.AccessRemoteDataStorageImpl
+import com.intelligentbackpack.accessdata.datasource.AccessLocalDataSourceImpl
+import com.intelligentbackpack.accessdata.datasource.AccessRemoteDataSourceImpl
 import com.intelligentbackpack.accessdata.storage.UserStorageImpl
 import com.intelligentbackpack.accessdomain.repository.AccessDomainRepository
 
@@ -15,10 +15,10 @@ object ServiceLocator {
         private set
 
     fun initializeRepository(context: Context) {
-        val accessRemoteDataStorage = AccessRemoteDataStorageImpl(accessUrl)
+        val accessRemoteDataSource = AccessRemoteDataSourceImpl(accessUrl)
         val userStorage = UserStorageImpl(context)
-        val accessLocalDataStorage = AccessLocalDataStorageImpl(userStorage)
-        accessRepository = AccessDomainRepositoryImpl(accessLocalDataStorage, accessRemoteDataStorage)
+        val accessLocalDataSource = AccessLocalDataSourceImpl(userStorage)
+        accessRepository = AccessDomainRepositoryImpl(accessLocalDataSource, accessRemoteDataSource)
     }
 
 
