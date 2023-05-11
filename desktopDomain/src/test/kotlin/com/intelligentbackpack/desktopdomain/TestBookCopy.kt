c26e9b092743c1018da5acc91a17200daf17350a
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-11 15:42:29 +0200

    test(desktop): remove tests of classes and logics removed

--- desktopDomain/src/test/kotlin/com/intelligentbackpack/desktopdomain/TestBookCopy.kt
+++ desktopDomain/src/test/kotlin/com/intelligentbackpack/desktopdomain/TestBookCopy.kt
@@ -10,15 +10,13 @@ class TestBookCopy : StringSpec({
 
     val title = "The Lord of the Rings"
     val authors = listOf("J. R. R. Tolkien")
-    val subjects = setOf("English")
     val rfidCode = "FF:24:3E:C1"
     val isbn = "978885152159X"
 
     "Create a book copy with a valid ISBN" {
-        val bookCopy = BookCopy.build(subjects) {
+        val bookCopy = BookCopy.build() {
             this.isbn = isbn
             this.rfidCode = rfidCode
-            this.subjects = subjects
             this.title = title
             this.authors = authors
         }
@@ -26,10 +24,9 @@ class TestBookCopy : StringSpec({
     }
 
     "Create a book copy with a valid RFID code" {
-        val bookCopy = BookCopy.build(subjects) {
+        val bookCopy = BookCopy.build {
             this.isbn = isbn
             this.rfidCode = rfidCode
-            this.subjects = subjects
             this.title = title
             this.authors = authors
         }
@@ -38,10 +35,9 @@ class TestBookCopy : StringSpec({
 
     "Create a book with a not valid ISBN missing a digit" {
         shouldThrow<ISBNException> {
-            BookCopy.build(subjects) {
+            BookCopy.build {
                 this.isbn = "978885152159"
                 this.rfidCode = rfidCode
-                this.subjects = subjects
                 this.title = title
                 this.authors = authors
             }
@@ -50,10 +46,9 @@ class TestBookCopy : StringSpec({
 
     "Create a book with a not valid ISBN with a letter, not X at the end" {
         shouldThrow<ISBNException> {
-            BookCopy.build(subjects) {
+            BookCopy.build {
                 this.isbn = "9788851521599"
                 this.rfidCode = rfidCode
-                this.subjects = subjects
                 this.title = title
                 this.authors = authors
             }
@@ -62,10 +57,9 @@ class TestBookCopy : StringSpec({
 
     "Create a book with a not valid ISBN with wrong check digit" {
         shouldThrow<ISBNException> {
-            BookCopy.build(subjects) {
+            BookCopy.build {
                 this.isbn = "97888515215A0"
                 this.rfidCode = rfidCode
-                this.subjects = subjects
                 this.title = title
                 this.authors = authors
             }
@@ -74,10 +68,9 @@ class TestBookCopy : StringSpec({
 
     "Create a book with an missing title" {
         shouldThrow<IllegalStateException> {
-            BookCopy.build(subjects) {
+            BookCopy.build() {
                 this.isbn = isbn
                 this.rfidCode = rfidCode
-                this.subjects = subjects
                 this.authors = authors
             }
         }
@@ -85,32 +78,19 @@ class TestBookCopy : StringSpec({
 
     "Create a book with an missing author" {
         shouldThrow<IllegalStateException> {
-            BookCopy.build(subjects) {
-                this.isbn = isbn
-                this.rfidCode = rfidCode
-                this.subjects = subjects
-                this.title = title
-            }
-        }
-    }
-
-    "Create a book with an missing subject" {
-        shouldThrow<IllegalStateException> {
-            BookCopy.build(subjects) {
+            BookCopy.build {
                 this.isbn = isbn
                 this.rfidCode = rfidCode
                 this.title = title
-                this.authors = authors
             }
         }
     }
 
     "Create a book with an empty title" {
         shouldThrow<IllegalStateException> {
-            BookCopy.build(setOf("English")) {
+            BookCopy.build() {
                 this.isbn = isbn
                 this.rfidCode = rfidCode
-                this.subjects = subjects
                 this.authors = authors
             }
         }
--- desktopDomain/src/test/kotlin/com/intelligentbackpack/desktopdomain/TestDesktop.kt
+++ desktopDomain/src/test/kotlin/com/intelligentbackpack/desktopdomain/TestDesktop.kt
@@ -1,13 +1,8 @@
 package com.intelligentbackpack.desktopdomain
 
-import com.intelligentbackpack.desktopdomain.entities.Backpack
 import com.intelligentbackpack.desktopdomain.entities.BookCopy
 import com.intelligentbackpack.desktopdomain.entities.Desktop
-import com.intelligentbackpack.desktopdomain.entities.GeneralSchoolSupply
 import com.intelligentbackpack.desktopdomain.entities.SchoolSupplyTypes
-import com.intelligentbackpack.desktopdomain.entities.Subjects
-import com.intelligentbackpack.desktopdomain.exception.AlreadyPresentType
-import com.intelligentbackpack.desktopdomain.exception.DuplicateBackpack
 import com.intelligentbackpack.desktopdomain.exception.DuplicateRFIDException
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
@@ -15,117 +10,39 @@ import io.kotest.matchers.shouldBe
 
 class TestDesktop : StringSpec({
 
-    val english = Subjects.create("English")
-    val subjects = setOf(english)
-    val pencilType = SchoolSupplyTypes.create("Pencil")
-    val bookCopy = BookCopy.build(subjects) {
+    val bookCopy = BookCopy.build() {
         isbn = "978885152159X"
         rfidCode = "FF:24:3E:C1"
-        this.subjects = subjects
         title = "The Lord of the Rings"
         authors = listOf("J. R. R. Tolkien")
     }
-    val pencil = GeneralSchoolSupply
-        .build(
-            listOf(pencilType),
-            subjects
-        ) {
-            rfidCode = "FF:24:3E:C2"
-            this.subjects = subjects
-            type = pencilType
-            description = "A pencil"
-        }
 
     "Create a Desktop" {
-        val backpack = Backpack.create("Backpack1")
-        val desktop = Desktop.create(setOf(backpack), setOf(bookCopy, pencil), setOf(pencilType), subjects)
-        desktop.backpacks.size shouldBe 1
-        desktop.backpacks.first() shouldBe backpack
-        desktop.schoolSupplies.size shouldBe 2
-        desktop.schoolSupplyTypes.size shouldBe 2
-        desktop.schoolSupplyTypes shouldBe setOf(SchoolSupplyTypes.BOOK, pencilType)
-    }
-
-    "Add a subject to Desktop" {
-        val desktop = Desktop.create(setOf(), setOf(bookCopy, pencil), setOf(pencilType), subjects)
-        val newSubject = Subjects.create("Math")
-        val newDesktop = desktop.addSubject(newSubject)
-        newDesktop.subjects.size shouldBe 3
-        newDesktop.subjects shouldBe (subjects + newSubject + Subjects.ALL).toSet()
-    }
-
-    "Add an already present subject to Desktop" {
-        val desktop = Desktop.create(setOf(), setOf(bookCopy, pencil), setOf(pencilType), subjects)
-        val newDesktop = desktop.addSubject(english)
-        newDesktop.subjects.size shouldBe 2
-        newDesktop.subjects shouldBe (subjects + Subjects.ALL).toSet()
-    }
-
-    "Add a School Supply type to Desktop" {
-        val desktop = Desktop.create(setOf(), setOf(bookCopy, pencil), setOf(pencilType), subjects)
-        val exerciseBookType = SchoolSupplyTypes.create("Exercise book")
-        val newDesktop = desktop.addSchoolSupplyType(exerciseBookType)
-        newDesktop.schoolSupplyTypes.size shouldBe 3
-        newDesktop.schoolSupplyTypes shouldBe setOf(pencilType, exerciseBookType, SchoolSupplyTypes.BOOK)
-    }
-
-    "Add an already present type to Desktop" {
-        val desktop = Desktop.create(setOf(), setOf(bookCopy, pencil), setOf(pencilType), subjects)
-        shouldThrow<AlreadyPresentType> {
-            desktop.addSchoolSupplyType(pencilType)
-        }
-    }
-
-    "Add a Backpack to Desktop" {
-        val desktop = Desktop.create(setOf(), setOf(bookCopy, pencil), setOf(pencilType), subjects)
-        val backpack = Backpack.create("Backpack1")
-        val newDesktop = desktop.addBackpack(backpack)
-        newDesktop.backpacks.size shouldBe 1
-        newDesktop.backpacks.first() shouldBe backpack
-    }
-
-    "Add an already present Backpack to Desktop" {
-        val backpack = Backpack.create("Backpack1")
-        val desktop = Desktop.create(setOf(backpack), setOf(bookCopy, pencil), setOf(pencilType), subjects)
-        shouldThrow<DuplicateBackpack> {
-            desktop.addBackpack(backpack)
-        }
+        val desktop = Desktop.create(setOf(bookCopy))
+        desktop.schoolSupplies.size shouldBe 1
+        desktop.schoolSupplyTypes.size shouldBe 1
+        desktop.schoolSupplyTypes shouldBe setOf(SchoolSupplyTypes.BOOK)
     }
 
     "Add a School Supply to Desktop" {
-        val backpack = Backpack.create("Backpack1")
-        val desktop = Desktop.create(setOf(backpack), setOf(bookCopy), setOf(pencilType), subjects)
-        val newDesktop = desktop.addSchoolSupply(pencil)
-        newDesktop.schoolSupplies.size shouldBe 2
-        newDesktop.schoolSupplies shouldBe setOf(bookCopy, pencil)
-    }
-
-    "Add an already present School Supply to Desktop" {
-        val backpack = Backpack.create("Backpack1")
-        val desktop = Desktop.create(setOf(backpack), setOf(bookCopy, pencil), setOf(pencilType), subjects)
-        shouldThrow<DuplicateRFIDException> {
-            desktop.addSchoolSupply(pencil)
-        }
-    }
-
-    "Replace a School Supply in Desktop" {
-        val backpack = Backpack.create("Backpack1")
-        val desktop = Desktop.create(setOf(backpack), setOf(bookCopy, pencil), setOf(pencilType), subjects)
-        val newBookCopy = BookCopy.build(subjects) {
+        val desktop = Desktop.create(setOf(bookCopy))
+        val bookCopy2 = BookCopy.build() {
             isbn = "978885152159X"
-            rfidCode = "FF:24:3E:C3"
-            this.subjects = subjects
+            rfidCode = "FF:24:3E:C2"
             title = "The Lord of the Rings"
             authors = listOf("J. R. R. Tolkien")
         }
-        val newDesktop = desktop.replaceSchoolSupplies(
-            setOf(newBookCopy), setOf(bookCopy)
-        )
-        newDesktop.schoolSupplies.size shouldBe 3
-        newDesktop.schoolSupplies shouldBe setOf(bookCopy, newBookCopy, pencil)
-        (newDesktop.schoolSupplies.first { it == bookCopy } as BookCopy).replacedBy shouldBe setOf(newBookCopy)
-        (newDesktop.schoolSupplies.first { it == bookCopy } as BookCopy).replace shouldBe setOf()
-        (newDesktop.schoolSupplies.first { it == newBookCopy } as BookCopy).replace shouldBe setOf(bookCopy)
-        (newDesktop.schoolSupplies.first { it == newBookCopy } as BookCopy).replacedBy shouldBe setOf()
+        val newDesktop = desktop.addSchoolSupply(bookCopy2)
+        desktop.schoolSupplies.size shouldBe 1
+        newDesktop.schoolSupplies.size shouldBe 2
+        newDesktop.schoolSupplyTypes.size shouldBe 1
+        newDesktop.schoolSupplyTypes shouldBe setOf(SchoolSupplyTypes.BOOK)
+    }
+
+    "Add an already present School Supply to Desktop" {
+        val desktop = Desktop.create(setOf(bookCopy))
+        shouldThrow<DuplicateRFIDException> {
+            desktop.addSchoolSupply(bookCopy)
+        }
     }
 })
