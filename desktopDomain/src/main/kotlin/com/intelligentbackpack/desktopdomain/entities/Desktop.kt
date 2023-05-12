fef820dd574c9b1889e5e8fa2482cad622abc092
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-12 18:01:31 +0200

    feat(desktop): add method to take and put supply from/into backpack

--- desktopDomain/src/main/kotlin/com/intelligentbackpack/desktopdomain/entities/Desktop.kt
+++ desktopDomain/src/main/kotlin/com/intelligentbackpack/desktopdomain/entities/Desktop.kt
@@ -20,6 +20,11 @@ interface Desktop {
     val schoolSupplyTypes: Set<SchoolSupplyType>
 
     /**
+     * The school supplies in the backpack.
+     */
+    val schoolSuppliesInBackpack: Set<SchoolSupply>
+
+    /**
      * Adds a school supply to the desktop.
      *
      * @param schoolSupply The school supply to add.
@@ -31,19 +36,38 @@ interface Desktop {
     @Throws(TypeException::class, DuplicateRFIDException::class)
     fun addSchoolSupply(schoolSupply: SchoolSupply): Desktop
 
+    /**
+     * Adds a school supply in the backpack.
+     *
+     * @param schoolSupply The school supply to add.
+     * @return The desktop with the school supply added.
+     */
+    fun putSchoolSupplyInBackpack(schoolSupply: SchoolSupply): Desktop
+
+    /**
+     * Extract a school supply from the backpack.
+     *
+     * @param schoolSupply The school supply to extract.
+     * @return The desktop with the school supply removed.
+     */
+    fun takeSchoolSupplyFromBackpack(schoolSupply: SchoolSupply): Desktop
+
     companion object {
         /**
          * Builds a desktop.
          *
          * @param schoolSupplies The school supplies of the desktop.
+         * @param schoolSuppliesInBackpack The school supplies in the backpack.
          * @return The desktop built.
          */
         fun create(
-            schoolSupplies: Set<SchoolSupply> = emptySet()
+            schoolSupplies: Set<SchoolSupply> = emptySet(),
+            schoolSuppliesInBackpack: Set<SchoolSupply> = emptySet()
         ): Desktop =
             DesktopImpl(
                 schoolSupplies,
                 setOf(SchoolSupplyTypes.BOOK),
+                schoolSuppliesInBackpack
             )
     }
 }
--- desktopDomain/src/main/kotlin/com/intelligentbackpack/desktopdomain/entities/implementations/DesktopImpl.kt
+++ desktopDomain/src/main/kotlin/com/intelligentbackpack/desktopdomain/entities/implementations/DesktopImpl.kt
@@ -3,8 +3,11 @@ package com.intelligentbackpack.desktopdomain.entities.implementations
 import com.intelligentbackpack.desktopdomain.entities.Desktop
 import com.intelligentbackpack.desktopdomain.entities.SchoolSupply
 import com.intelligentbackpack.desktopdomain.entities.SchoolSupplyType
+import com.intelligentbackpack.desktopdomain.exception.AlreadyInBackpackException
 import com.intelligentbackpack.desktopdomain.exception.DuplicateRFIDException
+import com.intelligentbackpack.desktopdomain.exception.SchoolSupplyNotFoundException
 import com.intelligentbackpack.desktopdomain.exception.TypeException
+import kotlin.jvm.Throws
 
 /**
  * Implementation of a desktop.
@@ -15,6 +18,7 @@ import com.intelligentbackpack.desktopdomain.exception.TypeException
 internal data class DesktopImpl(
     override val schoolSupplies: Set<SchoolSupply>,
     override val schoolSupplyTypes: Set<SchoolSupplyType>,
+    override val schoolSuppliesInBackpack: Set<SchoolSupply> = emptySet()
 ) : Desktop {
 
     /**
@@ -37,4 +41,47 @@ internal data class DesktopImpl(
                     schoolSupplyTypes = schoolSupplyTypes,
                 )
         }
+
+    /**
+     * Put a school supply in the backpack.
+     *
+     * @param schoolSupply The school supply to put in the backpack.
+     * @return A new desktop with the school supply in the backpack.
+     * @throws SchoolSupplyNotFoundException If the school supply is not in the desktop.
+     * @throws AlreadyInBackpackException If the school supply is already in the backpack.
+     */
+    @Throws(SchoolSupplyNotFoundException::class)
+    override fun putSchoolSupplyInBackpack(schoolSupply: SchoolSupply): Desktop {
+        if (!schoolSupplies.contains(schoolSupply))
+            throw SchoolSupplyNotFoundException(schoolSupply.rfidCode)
+        else {
+            if (schoolSuppliesInBackpack.contains(schoolSupply))
+                throw AlreadyInBackpackException(schoolSupply)
+            else
+                return DesktopImpl(
+                    schoolSupplies = schoolSupplies,
+                    schoolSupplyTypes = schoolSupplyTypes,
+                    schoolSuppliesInBackpack = schoolSuppliesInBackpack + schoolSupply
+                )
+        }
+    }
+
+    /**
+     * Take a school supply from the backpack.
+     *
+     * @param schoolSupply The school supply to take.
+     * @return A new desktop with the school supply taken from the backpack.
+     * @throws SchoolSupplyNotFoundException If the school supply is not in the backpack.
+     */
+    @Throws(SchoolSupplyNotFoundException::class)
+    override fun takeSchoolSupplyFromBackpack(schoolSupply: SchoolSupply): Desktop {
+        if (!schoolSuppliesInBackpack.contains(schoolSupply))
+            throw SchoolSupplyNotFoundException(schoolSupply.rfidCode)
+        else
+            return DesktopImpl(
+                schoolSupplies = schoolSupplies,
+                schoolSupplyTypes = schoolSupplyTypes,
+                schoolSuppliesInBackpack = schoolSuppliesInBackpack - schoolSupply
+            )
+    }
 }
