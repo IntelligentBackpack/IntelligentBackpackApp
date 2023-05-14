79383fbb4b33866b035cd6f7460777f39e25caeb
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-14 17:24:13 +0200

    feat(app): add BarcodeAnalyser

---
+++ app/src/main/kotlin/com/intelligentbackpack/app/sensor/BarcodeAnalyser.kt
@@ -0,0 +1,35 @@
+package com.intelligentbackpack.app.sensor
+
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+
+@androidx.annotation.OptIn(androidx.camera.core.ExperimentalGetImage::class)
+class BarcodeAnalyser(
+    val onBarcodeFound: (String) -> Unit,
+    val options: BarcodeScannerOptions = BarcodeScannerOptions.Builder()
+        .setBarcodeFormats(Barcode.FORMAT_QR_CODE, Barcode.FORMAT_EAN_13)
+        .build()
+) : ImageAnalysis.Analyzer {
+    override fun analyze(imageProxy: ImageProxy) {
+
+        val scanner = BarcodeScanning.getClient(options)
+        val mediaImage = imageProxy.image
+        mediaImage?.let {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+            scanner.process(image)
+                .addOnSuccessListener { barcodes ->
+                    if (barcodes.size > 0) {
+                        onBarcodeFound(barcodes[0].rawValue ?: "")
+                    }
+                }
+                .addOnFailureListener {
+                }
+        }
+        imageProxy.close()
+    }
+}
