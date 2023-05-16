db6416b32f0a5cbeea5a8cf603c88319fedba97f
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-16 15:05:28 +0200

    fix(app): in NavigationItem add route

--- app/src/main/kotlin/com/intelligentbackpack/app/ui/navigation/NavigationItem.kt
+++ app/src/main/kotlin/com/intelligentbackpack/app/ui/navigation/NavigationItem.kt
@@ -5,5 +5,6 @@ import androidx.compose.ui.graphics.vector.ImageVector
 class NavigationItem(
     val title: String,
     val icon: ImageVector,
+    val route: String = "",
     val action: () -> Unit
 )
