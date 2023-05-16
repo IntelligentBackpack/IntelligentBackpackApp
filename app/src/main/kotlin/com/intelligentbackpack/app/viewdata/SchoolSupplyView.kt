4980f0b351c3e7e85cf4d6ed30c1f05d5ebe7d41
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-16 20:01:30 +0200

    refactor(app): remove hierarchy from SchoolSupplyView

--- app/src/main/kotlin/com/intelligentbackpack/app/viewdata/SchoolSupplyView.kt
+++ app/src/main/kotlin/com/intelligentbackpack/app/viewdata/SchoolSupplyView.kt
@@ -2,15 +2,13 @@ package com.intelligentbackpack.app.viewdata
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import com.intelligentbackpack.desktopdomain.entities.SchoolSupply
-import com.intelligentbackpack.desktopdomain.entities.SchoolSupplyType
 
 /**
  * View data class for a school supply.
  */
 @Parcelize
 data class SchoolSupplyView(
-    override val rfidCode: String,
-    override val type: SchoolSupplyType,
+    val rfidCode: String,
+    val type: String,
     val book: BookView? = null
-) : SchoolSupply, Parcelable
+) : Parcelable
