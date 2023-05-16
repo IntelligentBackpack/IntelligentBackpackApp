ea5a4ecc6c07c48c12934dcf2049a93daa563c3e
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-16 12:46:21 +0200

    feat(app): App add desktopUseCase

--- app/src/main/kotlin/com/intelligentbackpack/app/App.kt
+++ app/src/main/kotlin/com/intelligentbackpack/app/App.kt
@@ -1,17 +1,30 @@
 package com.intelligentbackpack.app
 
 import android.app.Application
-import com.intelligentbackpack.accessdomain.usecase.AccessUseCase
 import com.intelligentbackpack.app.di.ServiceLocator
+import com.intelligentbackpack.accessdomain.usecase.AccessUseCase
+import com.intelligentbackpack.desktopdomain.usecase.DesktopUseCase
 
+/**
+ * Application class
+ */
 class App : Application() {
+    /**
+     * Access use case
+     */
+    lateinit var accessUseCase: AccessUseCase
+        private set
+
+    /**
+     * Desktop use case
+     */
+    lateinit var desktopUseCase: DesktopUseCase
+        private set
 
     override fun onCreate() {
         super.onCreate()
         ServiceLocator.initializeRepository(this)
         accessUseCase = AccessUseCase(ServiceLocator.accessRepository)
+        desktopUseCase = DesktopUseCase(accessUseCase, ServiceLocator.desktopRepository)
     }
-
-    lateinit var accessUseCase : AccessUseCase
-        private set
 }
