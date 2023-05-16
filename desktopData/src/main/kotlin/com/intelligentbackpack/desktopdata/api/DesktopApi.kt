dcef1e3409639a23af59f1b988c55e4bdb8053ad
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-16 12:25:22 +0200

    feat(desktop): add getLibrary and addBookCopy http request

--- desktopData/src/main/kotlin/com/intelligentbackpack/desktopdata/api/DesktopApi.kt
+++ desktopData/src/main/kotlin/com/intelligentbackpack/desktopdata/api/DesktopApi.kt
@@ -1,8 +1,12 @@
 package com.intelligentbackpack.desktopdata.api
 
 import book.communication.Book
+import book.communication.BuyBook
+import book.communication.Library
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 /**
@@ -12,7 +16,31 @@ import retrofit2.http.Query
  */
 interface DesktopApi {
 
+    /**
+     * Get the book data.
+     *
+     * @param isbn The ISBN of the book.
+     * @return The book data.
+     */
     @GET("/utility/getBook/")
     fun getBook(@Query("isbn") isbn: String): Call<Book>
+
+    /**
+     * Get the desktop data.
+     *
+     * @param email The email of the user.
+     * @return The desktop data.
+     */
+    @GET("/getCopy/Email")
+    fun getLibrary(@Query("email") email: String): Call<Library>
+
+    /**
+     * Add a book copy.
+     *
+     * @param buyBook The book copy to add.
+     * @return The result of the operation.
+     */
+    @PUT("/create/buyBook")
+    fun addBookCopy(@Body buyBook: BuyBook): Call<Void>
 
 }
