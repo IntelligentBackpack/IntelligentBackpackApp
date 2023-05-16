c8d9c896c7f24bd8837b1e01a95811f11c515c37
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-16 15:04:59 +0200

    feat(app): add Object TabNavigation

---
+++ app/src/main/kotlin/com/intelligentbackpack/app/ui/navigation/TabNavigation.kt
@@ -0,0 +1,15 @@
+package com.intelligentbackpack.app.ui.navigation
+
+/**
+ * TabNavigation is the navigation constants for the tab navigation.
+ */
+object TabNavigation {
+
+    const val schoolSupplies = "schoolSupplies"
+
+    const val calendar = "calendar"
+
+    const val backpack = "backpack"
+
+    const val forget = "forget"
+}
