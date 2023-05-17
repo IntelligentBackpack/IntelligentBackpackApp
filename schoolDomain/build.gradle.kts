8ac3371546cf4d5fed29de92cb0d22b457cf72c7
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-17 21:48:47 +0200

    build: add subproject schoolDomain

---
+++ schoolDomain/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java-library")
+    id("org.jetbrains.kotlin.jvm")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks {
+    withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            showCauses = true
+            showStackTraces = true
+            showStandardStreams = true
+            events(*org.gradle.api.tasks.testing.logging.TestLogEvent.values())
+        }
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            allWarningsAsErrors = true
+            freeCompilerArgs += listOf("-opt-in=kotlin.RequiresOptIn", "-Xinline-classes")
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":accessDomain"))
+    testImplementation(gradleTestKit())
+    testImplementation(libs.bundles.kotlin.testing)
+}
