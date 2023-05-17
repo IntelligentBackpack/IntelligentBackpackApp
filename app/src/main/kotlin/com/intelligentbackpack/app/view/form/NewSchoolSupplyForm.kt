a7331a28f8eb7814303be944968956a540382c6d
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-17 16:42:41 +0200

    fix(app): for insert ISBN use number keyboard

--- app/src/main/kotlin/com/intelligentbackpack/app/view/form/NewSchoolSupplyForm.kt
+++ app/src/main/kotlin/com/intelligentbackpack/app/view/form/NewSchoolSupplyForm.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PhotoCamera
 import androidx.compose.material.icons.outlined.Done
@@ -23,6 +24,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -91,6 +94,10 @@ fun NewSchoolSupplyForm(
                     Icon(imageVector = Icons.Filled.PhotoCamera, "scan isbn code")
                 }
             },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
         )
         book?.let {
             Row(
