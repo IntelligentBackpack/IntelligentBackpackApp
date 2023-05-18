3fb9018d7ff4d93e24e84d370ccba07f0a228419
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-18 22:23:23 +0200

    refactor(access): in AccessUseCase use Result<T> instead of callback

--- accessDomain/src/main/kotlin/com/intelligentbackpack/accessdomain/usecase/AccessUseCase.kt
+++ accessDomain/src/main/kotlin/com/intelligentbackpack/accessdomain/usecase/AccessUseCase.kt
@@ -13,18 +13,17 @@ class AccessUseCase(private val repository: AccessDomainRepository) {
     /**
      * Creates a new user.
      * @param user is the user to create.
-     * @param success is the success callback.
-     * @param error is the error callback.
+     * @return the result of the creation with the user.
      */
-    suspend fun createUser(user: User, success: (User) -> Unit, error: (Exception) -> Unit) =
+    suspend fun createUser(user: User): Result<User> =
         if (!repository.isUserLogged()) {
             try {
-                success(repository.createUser(user = user))
+                Result.success(repository.createUser(user = user))
             } catch (e: Exception) {
-                error(e)
+                Result.failure(e)
             }
         } else {
-            error(UserAlreadyLoggedException(user))
+            Result.failure(UserAlreadyLoggedException(user))
         }
 
     /**
@@ -32,70 +31,60 @@ class AccessUseCase(private val repository: AccessDomainRepository) {
      *
      * @param email is the user email.
      * @param password is the user password.
-     * @param success is the success callback with the logged user.
-     * @param error is the error callback.
+     * @return the result of the login with the user.
      */
-    suspend fun loginWithData(
-        email: String,
-        password: String,
-        success: suspend (User) -> Unit,
-        error: (Exception) -> Unit,
-    ) =
+    suspend fun loginWithData(email: String, password: String): Result<User> =
         try {
-            success(repository.loginWithData(email, password))
+            Result.success(repository.loginWithData(email, password))
         } catch (e: Exception) {
-            error(e)
+            Result.failure(e)
         }
 
     /**
      * Logs the saved user.
      *
-     * @param success is the success callback with the logged user.
-     * @param error is the error callback.
+     * @return the result of the login with the user.
      */
-    suspend fun automaticLogin(success: suspend (User) -> Unit, error: (Exception) -> Unit) =
+    suspend fun automaticLogin(): Result<User> =
         try {
-            success(repository.automaticLogin())
+            Result.success(repository.automaticLogin())
         } catch (e: Exception) {
-            error(e)
+            Result.failure(e)
         }
 
     /**
      * Gets the logged user.
      *
-     * @param success is the success callback with the logged user.
-     * @param error is the error callback.
+     * @return the user.
      */
-    suspend fun getLoggedUser(success: suspend (User) -> Unit, error: (Exception) -> Unit) =
+    suspend fun getLoggedUser(): Result<User> =
         try {
-            success(repository.getLoggedUser())
+            Result.success(repository.getLoggedUser())
         } catch (e: Exception) {
-            error(e)
+            Result.failure(e)
         }
 
     /**
      * Logs out the user.
      *
-     * @param success is the success callback.
-     * @param error is the error callback.
+     * @return the result of the logout with the user.
      */
-    suspend fun logoutUser(success: (User) -> Unit, error: (Exception) -> Unit) =
+    suspend fun logoutUser(): Result<User> =
         try {
-            success(repository.logoutUser())
+            Result.success(repository.logoutUser())
         } catch (e: Exception) {
-            error(e)
+            Result.failure(e)
         }
 
     /**
      * Deletes the user.
      *
-     * @param success is the success callback.
-     * @param error is the error callback.
+     * @return the result of the deletion with the user.
      */
-    suspend fun deleteUser(success: (User) -> Unit, error: (Exception) -> Unit) =
+    suspend fun deleteUser(): Result<User> =
         try {
-            success(repository.deleteUser())
+            Result.success(repository.deleteUser())
         } catch (e: Exception) {
-            error(e)
+            Result.failure(e)
         }
 }
