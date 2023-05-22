aa5b558826cf8e4e6f1045182e659bb5aaf85f78
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-05-22 10:21:47 +0200

    fix(network): increase timeout time

--- networkUtility/src/main/java/com/intelligentbackpack/networkutility/RetrofitHelper.kt
+++ networkUtility/src/main/java/com/intelligentbackpack/networkutility/RetrofitHelper.kt
@@ -3,6 +3,7 @@ package com.intelligentbackpack.networkutility
 import io.github.andreabrighi.converter.RetrofitJsonConverterFactory
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
 
 /**
  * RetrofitHelper is an helper to create a Retrofit instance.
@@ -15,6 +16,8 @@ object RetrofitHelper {
      */
     fun getInstance(baseUrl: String): Retrofit {
         val httpClient = OkHttpClient.Builder()
+            .readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
         return Retrofit
             .Builder()
             .baseUrl(baseUrl)
