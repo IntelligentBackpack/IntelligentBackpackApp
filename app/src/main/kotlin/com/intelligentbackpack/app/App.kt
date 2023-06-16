f339fdc269a2234e22a82ebabb70f46ffbc5668d
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2023-06-16 20:32:13 +0200

    feat(app): add schoolUseCase and reminderUseCase

--- app/src/main/kotlin/com/intelligentbackpack/app/App.kt
+++ app/src/main/kotlin/com/intelligentbackpack/app/App.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import com.intelligentbackpack.accessdomain.usecase.AccessUseCase
 import com.intelligentbackpack.app.di.ServiceLocator
 import com.intelligentbackpack.desktopdomain.usecase.DesktopUseCase
+import com.intelligentbackpack.reminderdomain.usecase.ReminderUseCase
+import com.intelligentbackpack.schooldomain.usecase.SchoolUseCase
 
 /**
  * Application class
@@ -21,13 +23,32 @@ class App : Application() {
     lateinit var desktopUseCase: DesktopUseCase
         private set
 
+    lateinit var schoolUseCase: SchoolUseCase
+        private set
+
+    lateinit var reminderUseCase: ReminderUseCase
+        private set
+
     override fun onCreate() {
         super.onCreate()
         ServiceLocator.initializeRepository(this)
         accessUseCase = AccessUseCase(ServiceLocator.accessRepository)
         desktopUseCase = DesktopUseCase(accessUseCase, ServiceLocator.desktopRepository)
-        accessUseCase.onUserLogin = { desktopUseCase.downloadDesktop() }
-        accessUseCase.onUserLogout = { desktopUseCase.logoutDesktop() }
+        schoolUseCase = SchoolUseCase(accessUseCase, ServiceLocator.schoolRepository)
+        reminderUseCase = ReminderUseCase(
+            accessUseCase,
+            desktopUseCase,
+            schoolUseCase,
+            ServiceLocator.reminderRepository,
+        )
+        accessUseCase.onUserLogin = {
+            desktopUseCase.downloadDesktop()
+            schoolUseCase.downloadSchool()
+            reminderUseCase.downloadReminder()
+        }
+        accessUseCase.onUserLogout = {
+            desktopUseCase.logoutDesktop()
+        }
         accessUseCase.onUserDelete = { desktopUseCase.deleteDesktop() }
     }
 }
